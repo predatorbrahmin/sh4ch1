ec51f4c9ca450e67e03e829beea6c8810733fdf7
Divyansh Pandiit <70248263+predatorbrahmin@users.noreply.github.com>
2020-09-01 17:33:21 +0530

    Update fbvisit.sh

--- fbvisit.sh
+++ fbvisit.sh
@@ -4,7 +4,7 @@ echo SH4CH1 +-+-+-+-+-+-+-SH4CH1+-+-
 echo +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
  
 echo -e " press one to enter facebook "
-sleep 3 
+sleep 5
 echo "Loading fb...."
 sleep 10
 w3m www.facebook.com
