efc35085c5751dee20a50725a2a2cd055f2b8566
Divyansh Pandiit <70248263+predatorbrahmin@users.noreply.github.com>
2020-09-01 17:56:13 +0530

    Update sh4ch1.sh

--- sh4ch1.sh
+++ sh4ch1.sh
@@ -1,9 +1,13 @@
 red="\033[1;31m"
 green="\033[1;32m"
+yellow="\033[1;33m"
 echo -e "$red" 
 echo "+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+" 
 echo "SH4CH1 +-+-+-+-+-+-+-SH4CH1+-+-"
 echo "+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+"
+echo -e "$yellow"
+echo "YOU ARE ENTERING IN THE PROGRAM SH4CHI 2.0 A VIRTUAL FB"
+sleep 2
 echo -e "$green"
 echo -e " press one to enter facebook "
 sleep 5
