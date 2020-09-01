31b6c6fc2dc822188549a292dff6d7701121c44e
Divyansh Pandiit <70248263+predatorbrahmin@users.noreply.github.com>
2020-09-01 17:21:49 +0530

    Update fbvisit.sh

--- fbvisit.sh
+++ fbvisit.sh
@@ -1,9 +1,14 @@
+echo  ^    ^    ^    ^    ^    ^ 
+echo /S\  /H\  /A\  /C\  /H\  /I\
+echo <___><___><___><___><___><___> 2.0
+
 echo +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+ 
 echo divyansh pandit 
 echo +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
-echo -e "$yellow Select From Menu : $nc"
  
 echo " press one to enter facebook "
-sleep 15
+sleep 3 
+echo "Loading fb....
+sleep 10
 w3m www.facebook.com
 
