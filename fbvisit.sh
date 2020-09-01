93c6b2c233a3f08e4d35dbd86672c9e5937b461a
Divyansh Pandiit <70248263+predatorbrahmin@users.noreply.github.com>
2020-09-01 16:48:38 +0530

    Update fbvisit.sh

--- fbvisit.sh
+++ fbvisit.sh
@@ -1,5 +1,6 @@
 apt install w3m
- echo +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+ |d|i|v|y|a|n|s|h| |p|a|n|d|i|7|
+ echo +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+ 
+ echo divyansh pandit 
  echo +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
 
 w3m www.facebook.com
