a79cdd745486b25fb6dab79f5b85adeb0dc6a43c
Divyansh Pandiit <70248263+predatorbrahmin@users.noreply.github.com>
2020-09-01 17:15:36 +0530

    Update fbvisit.sh

--- fbvisit.sh
+++ fbvisit.sh
@@ -1,13 +1,9 @@
-apt install w3m
- echo +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+ 
- echo divyansh pandit 
- echo +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
+echo +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+ 
+echo divyansh pandit 
+echo +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
 echo -e "$yellow Select From Menu : $nc"
  
 echo " press one to enter facebook "
-read -p "Wanna Back To Main Menu [ Y / n ] : " check2
-echo -e "$nc"
-if [ $check2 = "1" ]; then
+sleep 15
 w3m www.facebook.com
-else 
-exit
+
