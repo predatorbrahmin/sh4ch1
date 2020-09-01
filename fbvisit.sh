8a061476dcd3f3f27aabf0033a0e255ad4c26db5
Divyansh Pandiit <70248263+predatorbrahmin@users.noreply.github.com>
2020-09-01 16:57:32 +0530

    Update fbvisit.sh

--- fbvisit.sh
+++ fbvisit.sh
@@ -2,5 +2,11 @@ apt install w3m
  echo +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+ 
  echo divyansh pandit 
  echo +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
+echo -e "$yellow Select From Menu : $nc"
 
+echo -e "		$Cyan 1 : Facebook $nc"
+
+echo -e "		$Cyan 99: Exit$nc"
+read -p "Choice >  " ch
+if [ $ch = 1 ]; then
 w3m www.facebook.com
